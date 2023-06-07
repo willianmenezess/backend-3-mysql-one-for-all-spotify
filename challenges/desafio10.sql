122aa91e17438f922cfd143adb3c7d46da51ce15
willianmenezess <willmenezes@outlook.com.br>
2023-06-07 18:58:51 -0300

    resolve requisito 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -11,3 +11,27 @@
 --   ('exemplo de dados 1', 'exemplo de dados A'),
 --   ('exemplo de dados 2', 'exemplo de dados B'),
 --   ('exemplo de dados 3', 'exemplo de dados C');
+
+
+CREATE TABLE SpotifyClone.usuario_cancoes_fav(
+    usuario_id INT NOT NULL,
+    cancao_id INT NOT NULL,
+    CONSTRAINT pk_usuario_cancoes_favoritas PRIMARY KEY (usuario_id, cancao_id),
+        FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id),
+        FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes(cancao_id)
+) engine = InnoDB;
+INSERT INTO SpotifyClone.usuario_cancoes_fav (usuario_id, cancao_id)
+VALUES
+    (1, 3),
+    (1, 6),
+    (1, 10),
+    (2, 4),
+    (3, 1),
+    (3, 3),
+    (4, 7),
+    (4, 4),
+    (5, 10),
+    (5, 2),
+    (8, 4),
+    (9, 7),
+    (10, 3);
