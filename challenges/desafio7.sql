37699be2fbf01f4af8c1471a4fd561c42d6746d4
willianmenezess <willmenezes@outlook.com.br>
2023-06-07 15:27:22 -0300

    fix: refatora req 7

--- challenges/desafio7.sql
+++ challenges/desafio7.sql
@@ -1,15 +1,11 @@
-SELECT
-  a.artista_nome AS artista,
-  al.album_titulo AS album,
-  COUNT(DISTINCT sa.usuario_id) AS pessoas_seguidoras
-FROM
-  SpotifyClone.artista a
-  INNER JOIN SpotifyClone.album al ON a.artista_id = al.artista_id
-  LEFT JOIN SpotifyClone.seguindo_artistas sa ON a.artista_id = sa.artista_id
-GROUP BY
-  a.artista_id,
-  al.album_id
-ORDER BY
-  pessoas_seguidoras DESC,
-  artista,
-  album;
+SELECT 
+	ar.artista_nome AS artista,
+    al.album_titulo AS album,
+    COUNT(sa.usuario_id) AS pessoas_seguidoras
+FROM SpotifyClone.artista ar
+INNER JOIN SpotifyClone.album al ON ar.artista_id = al.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas sa ON ar.artista_id = sa.artista_id
+GROUP BY 
+	ar.artista_id,
+	al.album_id
+ORDER BY pessoas_seguidoras DESC, ar.artista_nome, al.album_titulo;
